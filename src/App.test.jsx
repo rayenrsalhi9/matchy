c7e82bfbbe6019e635fd599ba4af041eb49dd816
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-18 18:20:27 +0100

    test: add initial test for App component play button
    Add test case to verify the App component renders 16 images when the play game button is clicked

---
+++ src/App.test.jsx
@@ -0,0 +1,20 @@
+import { describe, test, expect } from "vitest";
+import { screen, render } from "@testing-library/react";
+import App from './App'
+import userEvent from "@testing-library/user-event";
+
+describe('App', () => {
+
+    test('handles play game button click', async () => {
+
+        const user = userEvent.setup()
+        render(<App />)
+        const playGameBtn = screen.getByRole('button')
+
+        await user.click(playGameBtn)
+
+        const images = screen.getAllByRole('img')
+        expect(images).toHaveLength(16)
+    })
+
+})
