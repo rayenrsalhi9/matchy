bec91c7247a6d4e00709844b3713ab96706d65ed
Salhi Rayen <rayen.rsalhi9@gmail.com>
2025-08-18 02:43:16 +0100

    refactor(cards): update image paths to remove public prefix

--- src/cards.js
+++ src/cards.js
@@ -1,97 +1,97 @@
 export const cards = [
   {
     id: 1,
-    img: { url: "/public/images/game/apple.png", alt: "Apple" },
+    img: { url: "/images/game/apple.png", alt: "Apple" },
     value: "apple",
     isRevealed: false
   },
   {
     id: 2,
-    img: { url: "/public/images/game/apple.png", alt: "Apple" },
+    img: { url: "/images/game/apple.png", alt: "Apple" },
     value: "apple",
     isRevealed: false
   },
   {
     id: 3,
-    img: { url: "/public/images/game/apricot.png", alt: "Apricot" },
+    img: { url: "/images/game/apricot.png", alt: "Apricot" },
     value: "apricot",
     isRevealed: false
   },
   {
     id: 4,
-    img: { url: "/public/images/game/apricot.png", alt: "Apricot" },
+    img: { url: "/images/game/apricot.png", alt: "Apricot" },
     value: "apricot",
     isRevealed: false
   },
   {
     id: 5,
-    img: { url: "/public/images/game/grape.png", alt: "Grape" },
+    img: { url: "/images/game/grape.png", alt: "Grape" },
     value: "grape",
     isRevealed: false
   },
   {
     id: 6,
-    img: { url: "/public/images/game/grape.png", alt: "Grape" },  
+    img: { url: "/images/game/grape.png", alt: "Grape" },  
     value: "grape",
     isRevealed: false
   },
   {
     id: 7,
-    img: { url: "/public/images/game/melon.png", alt: "Melon" },
+    img: { url: "/images/game/melon.png", alt: "Melon" },
     value: "melon",
     isRevealed: false
   },
   {
     id: 8,
-    img: { url: "/public/images/game/melon.png", alt: "Melon" },
+    img: { url: "/images/game/melon.png", alt: "Melon" },
     value: "melon",
     isRevealed: false
   },
   {
     id: 9,
-    img: { url: "/public/images/game/orange.png", alt: "Orange" },
+    img: { url: "/images/game/orange.png", alt: "Orange" },
     value: "orange",
     isRevealed: false
   },
   {
     id: 10,
-    img: { url: "/public/images/game/orange.png", alt: "Orange" },
+    img: { url: "/images/game/orange.png", alt: "Orange" },
     value: "orange",
     isRevealed: false
   },
   {
     id: 11,
-    img: { url: "/public/images/game/peach.png", alt: "Peach" },
+    img: { url: "/images/game/peach.png", alt: "Peach" },
     value: "peach",
     isRevealed: false
   },
   {
     id: 12,
-    img: { url: "/public/images/game/peach.png", alt: "Peach" },
+    img: { url: "/images/game/peach.png", alt: "Peach" },
     value: "peach",
     isRevealed: false
   },
   {
     id: 13,
-    img: { url: "/public/images/game/pineapple.png", alt: "Pineapple" },
+    img: { url: "/images/game/pineapple.png", alt: "Pineapple" },
     value: "pineapple",
     isRevealed: false
   },
   {
     id: 14,
-    img: { url: "/public/images/game/pineapple.png", alt: "Pineapple" },
+    img: { url: "/images/game/pineapple.png", alt: "Pineapple" },
     value: "pineapple",
     isRevealed: false
   },
   {
     id: 15,
-    img: { url: "/public/images/game/strawberry.png", alt: "Strawberry" },
+    img: { url: "/images/game/strawberry.png", alt: "Strawberry" },
     value: "strawberry",
     isRevealed: false
   },
   {
     id: 16,
-    img: { url: "/public/images/game/strawberry.png", alt: "Strawberry" },
+    img: { url: "/images/game/strawberry.png", alt: "Strawberry" },
     value: "strawberry",
     isRevealed: false
   },
